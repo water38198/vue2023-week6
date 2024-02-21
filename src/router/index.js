e94d85cd131e76b3306b6fa1e6f4dfe23a3a7ab0
PayRoom <water38198@gmail.com>
2024-02-21 13:27:46 +0800

    修正plugin-vue_export-helpe問題

--- src/router/index.js
+++ src/router/index.js
@@ -9,55 +9,46 @@ const router = createRouter({
       children: [
         {
           path: '',
-          component: ()=>import('../views/front/HomeView.vue')
+          component: () => import('../views/front/HomeView.vue')
         },
         {
           path: 'about',
-          name: 'about',
           component: () => import('../views/front/AboutView.vue')
         },
         {
           path: 'products',
-          name: 'products',
           component: () => import('../views/front/ProductsView.vue')
         },
         {
           path: 'carts',
-          name: 'carts',
           component: () => import('../views/front/CartView.vue')
         }
       ]
     },
     {
       path: '/admin',
-      name: 'dashboard',
       component: () => import('../views/DashboardView.vue'),
       children: [
         {
           path: 'products',
-          name: 'adminProducts',
           component: () => import('../views/dashboard/ProductsView.vue')
         },
         {
           path: 'orders',
-          name: 'orders',
           component: () => import('../views/dashboard/OrdersView.vue')
         },
         {
           path: 'coupons',
-          name: 'coupons',
           component: () => import('../views/dashboard/CouponsView.vue')
         }
       ]
     },
     {
       path: '/login',
-      name: 'login',
       component: () => import('../views/LoginView.vue')
     },
     {
       path: '/:pathMatch(.*)*',
-      name: 'NotFound',
       component: () => import('../views/NotFound.vue')
     },
   ]
--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,38 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 // https://vitejs.dev/config/
+  /* global process */
+
 export default defineConfig({
   plugins: [
     vue(),
     UnoCSS(),
   ],
-  /* global process */
-  base: process.env.NODE_ENV === "production" ? "/vue2023-week6/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/vue2023-week6/" : "./",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // 錯誤修正 參考:https://israynotarray.com/other/20220928/1377668819/
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : '';
+          // substr 是被淘汰語法，因此要改 slice
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
+  },
 })
